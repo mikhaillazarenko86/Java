dbb8741c2d0bb6401d1d571e895bc1bafdb2d26e
mikhaillazarenko86 <lazarenko.mihail@gmail.com>
2023-03-25 14:39:31 +0300

    Error

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        private static boolean isSumBetween10And20(int a, int b){
+            int sum = a + b;
+            if (sum >= 10 && sum <= 20){
+                return true;
+            }
+            else {
+                return false;
+            }
+
+
+        }
+        System.out.println(isSumBetween10And20(5, 15));
+    }
+//      int a = 2;
+//      int b = 15;
+//      boolean c = true;
+//      boolean d = false;
+//      int sum = a + b;
+//      if (sum > 10 && sum < 20){
+//          System.out.println(c);
+//      }
+//      else {
+//          System.out.println(d);
+//      }
+//
+//    }
+}
