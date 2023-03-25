96e6738599e44972084fa68f13785ad505f63a8a
mikhaillazarenko86 <lazarenko.mihail@gmail.com>
2023-03-25 14:43:30 +0300

    Error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void wer(String[] args) {
 
         private static boolean isSumBetween10And20(int a, int b){
             int sum = a + b;
