21a2bd3c692cbc2f8f8840c02df6d9fc951d68e2
mikhaillazarenko86 <lazarenko.mihail@gmail.com>
2023-03-26 10:08:05 +0300

    finished homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,71 +19,112 @@ public class Main {
         int[] array = new int[]{0, 1, 0, 1, 1, 1, 0, 0, 0};
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(changeArray(array)));
-        }
+        int[] arraySecond = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(arraySecond));
+        System.out.println(Arrays.toString(multiplicationArray(arraySecond)));
+        matrix(4);
+
+    }
+
     /**
      * 1. Написать метод, принимающий на вход два целых числа и проверяющий,
      * что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
      */
-        private static boolean isSumBetween10And20(int a, int b){
+    private static boolean isSumBetween10And20(int a, int b) {
         int sum = a + b;
-        if (sum >= 10 && sum <= 20){
+        if (sum >= 10 && sum <= 20) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
 
     }
-    private static boolean isPositive(int x){
-        if (x >= 0){
+
+    private static boolean isPositive(int x) {
+        if (x >= 0) {
             return true;
         } else {
             return false;
         }
     }
+
     private static void printString(String source, int repeat) {
         // напечатать строку source repeat раз
         int count = 0;
-        while (repeat != count){
+        while (repeat != count) {
             System.out.print(source);
             count++;
         }
     }
+
     private static boolean isLeapYear(int year) {
         // проверить, является ли год високосным. если да - return true
         // год является високосным, если он делится на 4, но не делится на 100, но делится на 400
-       if (year % 4 == 0 && year % 100 !=0 && year % 400 != 0){
-           return true;
-       }
-           else {
-               return false;
-           }
-       }
+        if (year % 4 == 0 && year % 100 != 0 && year % 400 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private static int[] createArray(int len, int initialValue) {
         // должен вернуть массив длины len, каждое значение которого равно initialValue
-        int[] array = new int [len];
-        for (int i = 0; i < len; i++){
-            array [i] = initialValue;
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
         }
         return array;
     }
-//    1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+
+    //    1. Задать целочисленный массив, состоящий из элементов 0 и 1.
 //            * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
-    private static int[] changeArray(int[] array){
-            for (int i = 0; i < array.length; i++){
-                if (array[i] == 1){
-                    array[i] = 0;
+    private static int[] changeArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                array[i] = 0;
+            } else {
+                array[i] = 1;
+            }
+        }
+        return array;
+    }
+
+    //2. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+    private static int[] multiplicationArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] = array[i] * 2;
+            }
+
+        }
+        return array;
+    }
+    //    3. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы
+    //    единицами (можно только одну из диагоналей, если обе сложно).
+    //            * Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+    // Также заполнить элементы побочной диагонали
+    private static void matrix(int length){
+        int[][] matrix = new int[length][length];
+        for (int i = 0; i < length; i++){
+            System.out.println();
+            for (int j = 0; j < length; j++){
+                if (i == j || j == length - 1 - i){
+                    matrix[i][j] = 1;
                 }
                 else{
-                    array[i] = 1;
+                    matrix[i][j] = 0;
                 }
             }
-            return array;
+
+        }
+        for (int i = 0; i < length; i++) {
+            System.out.println();
+            for (int j = 0; j < length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+        }
     }
-
-
-    }
-
+}
 
 
 //      int a = 2;
